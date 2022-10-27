d6ed9ce3d5f4e4695363c819bef09ebc7e97f591
Leyla Kandalova <leyla_kandalova@epam.com>
2022-10-27 20:09:01 +0400

    Fix logo import

--- src/components/Header/components/Logo.tsx
+++ src/components/Header/components/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from '../../../../public/img/logo.png';
+import logo from '../../../assets/img/logo.png';
 
 export function Logo() {
 	return (
 		<img
-			src={process.env.PUBLIC_URL + '/img/logo.png'}
+			src={logo}
+			// src={process.env.PUBLIC_URL + '/img/logo.png'}
 			// src='https://play-lh.googleusercontent.com/JmTQsXdAWCQqJlm4BeKlxV_9gc5pt89YBIRq398VXR3Nk4qVlak25sVQlfrzJrpulpA=w480-h960-rw'
 			alt='My logo'
 		/>
