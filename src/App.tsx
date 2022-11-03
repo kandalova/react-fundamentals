29dc244d54d59a2f79f8d4fbec95a337a3c59506
Leyla Kandalova <leyla_kandalova@epam.com>
2022-11-03 13:00:02 +0400

    Fix redirect by default

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
 								/>
 							}
 						/>
-						<Route path='*' element={<Navigate to='/' />} />
+						<Route path='*' element={<Navigate to='/courses' />} />
 					</Routes>
 				</div>
 			</AuthorsContext.Provider>
