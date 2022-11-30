e0dc7b0e27ba450587975325e0a118ddeefaa7b1
Leyla Kandalova <leyla_kandalova@epam.com>
2022-11-30 20:39:29 +0400

    Tests for header

---
+++ src/components/__tests__/Header.spec.tsx
@@ -0,0 +1,42 @@
+import { renderApp } from '../../utils/renderApp';
+import { Header } from '../Header/Header';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+
+jest.mock('react-redux', () => {
+	const originalModule = jest.requireActual('react-redux');
+
+	return {
+		...originalModule,
+		useDispatch: () => jest.fn(),
+	};
+});
+
+describe('Header', () => {
+	it('Component should render header with username', () => {
+		const testUser = {
+			isAuth: true,
+			name: 'TestUser',
+		};
+		renderApp(<Header />, {
+			user: { userInfo: testUser },
+		});
+
+		expect(screen.getByAltText('My logo')).toBeVisible();
+		expect(screen.getByText(testUser.name)).toBeVisible();
+	});
+
+	it('Component should render header without username', () => {
+		const testUser = {
+			isAuth: false,
+			name: 'TestUser',
+		};
+		renderApp(<Header />, {
+			user: { userInfo: testUser },
+		});
+
+		expect(screen.getByAltText('My logo')).toBeVisible();
+		expect(screen.queryByText(testUser.name)).toBeNull();
+	});
+});
