b6768e7516734b8058d0d23b7fa7d14b980d3d09
Leyla Kandalova <leyla_kandalova@epam.com>
2022-12-02 20:48:55 +0400

    Fix store for tests

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,7 @@ import { rootReducer, StoreState } from './rootReducer';
 export function createStore(preloadedState?: DeepPartial<StoreState>) {
 	return configureStore({
 		reducer: rootReducer,
-		preloadedState,
+		// To fix TS warning, preloaded state should only be used in test env
+		preloadedState: preloadedState as StoreState,
 	});
 }
